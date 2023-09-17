26c1f36f1a5974b163b6da717cc414a38bc1dc9f
sevyor1 <usa.umidovna@gmail.com>
2023-09-17 13:17:29 +0500

    initial

---
+++ src/main/java/com/sv/Mapper/and/Generic/MapperAndGenericApplication.java
@@ -0,0 +1,13 @@
+package com.sv.Mapper.and.Generic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapperAndGenericApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapperAndGenericApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sv/Mapper/and/Generic/MapperAndGenericApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sv.Mapper.and.Generic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapperAndGenericApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
